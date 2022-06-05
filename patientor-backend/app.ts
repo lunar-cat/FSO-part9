5d4101546f3ed84bf981874b3be0eb52ea6514cd
Jose Pradenas Navarro <josepradenasnavarro@gmail.com>
2022-06-05 17:26:07 -0400

    exercise 9.9

--- patientor-backend/app.ts
+++ patientor-backend/app.ts
@@ -1,6 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+const allowedOrigins = ["http://localhost:3000"];
+const options: cors.CorsOptions = { origin: allowedOrigins };
+app.use(cors(options));
 app.use(express.json());
 
 app.get("/api/ping", (_req, res) => {
--- patientor-backend/index.ts
+++ patientor-backend/index.ts
@@ -2,7 +2,7 @@ import http from "http";
 import app from "./app";
 
 const server = http.createServer(app);
-const PORT = 3000;
+const PORT = 3001;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
