610c58aa385d4bb9884968ae3a43a4f73ee6d018
Jose Pradenas Navarro <josepradenasnavarro@gmail.com>
2022-06-08 18:33:33 -0400

    (add): backend Entry types && changed patients initial data

--- patientor-backend/src/services/diagnosisService.ts
+++ patientor-backend/src/services/diagnosisService.ts
@@ -1,7 +1,7 @@
 import diagnoses from "../../data/diagnoses";
-import { Diagnose } from "../types";
+import { Diagnosis } from "../types";
 
-const getDiagnoses = (): Diagnose[] => {
+const getDiagnoses = (): Diagnosis[] => {
   return diagnoses;
 };
 
--- patientor-backend/src/types.ts
+++ patientor-backend/src/types.ts
@@ -1,4 +1,4 @@
-export interface Diagnose {
+export interface Diagnosis {
   code: string;
   name: string;
   latin?: string;
@@ -8,8 +8,36 @@ export enum Gender {
   Female = "female",
   Other = "other",
 }
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {}
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: { startDate: string; endDate: string };
+}
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: { date: string; criteria: string };
+}
+export type Entry =
+  | HealthCheckEntry
+  | OccupationalHealthcareEntry
+  | HospitalEntry;
 export interface Patient {
   id: string;
   name: string;
